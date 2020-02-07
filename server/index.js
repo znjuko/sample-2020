ae3b70469b4407eb5db5e51c7c4f32f888e8ad73
znjuko <znajderko@yandex.ru>
2020-02-07 20:45:17 +0300

    pushed for check

--- server/index.js
+++ server/index.js
@@ -8,18 +8,20 @@
 
 const http = require('http');
 const fs = require('fs');
+const debug = require('debug');
+
+const log = debug('*');
 
 const server = http.createServer(
     (req, res) => {
 
-
+        // почитать про шаблонные строки js 
         console.log('url : ', req.url);
-        const filename = req.url === '/' ? req.url + 'index.html' : req.url ;
+        const filename = req.url === '/' ? req.url + 'index.html' : req.url;
 
-        fs.readFile(`public${filename}`, (err,body) => {
-            if(err)
-            {
-                console.log('Error : ',err);
+        fs.readFile(`public${filename}`, (err, body) => {
+            if (err) {
+                log('Error : ', err);
                 res.write('404');
 
                 res.end();
@@ -27,12 +29,13 @@ const server = http.createServer(
             }
 
             res.write(body);
+            log('OK', filename);
 
             res.end();
         });
 
 
-        
+
 
         // сервер записал в заголовок ответ , но не закончил соеднинение
         // необходимо закрыть запрос
